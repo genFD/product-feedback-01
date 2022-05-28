caecffb6fd18a957b9f215001647650b9450dc26
hermkan <hermann.kanga.dev@gmail.com>
2022-05-27 20:52:34 -0400

    establish connection with db

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,24 @@ const dotenv = require('dotenv').config();
 const colors = require('colors');
 const userRoutes = require('./routes/user.routes.js');
 const { handleError } = require('./middleware/error.js');
-
+const { connectDatabase } = require('./config/database.js');
 const app = express();
 app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use(handleError);
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(
-    `Server is listening on ${PORT}
-  `.yellow.bold
-  );
-});
+const startServer = async () => {
+  try {
+    await connectDatabase();
+    app.listen(
+      PORT,
+      console.log(
+        `SERVER IS LISTENING ON PORT ${PORT}`.bgGreen.black.underline.bold
+      )
+    );
+  } catch (error) {
+    console.log(`Cannot start server : ${error}`.bgRed.underline.bold);
+  }
+};
+startServer();
