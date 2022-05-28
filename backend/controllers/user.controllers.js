5dc641a15df227b85f490ae087a77a13c66a6901
hermkan <hermann.kanga.dev@gmail.com>
2022-05-27 21:46:12 -0400

    completed login and create jwt

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -2,9 +2,25 @@ const { StatusCodes } = require('http-status-codes');
 const handleAsync = require('express-async-handler');
 const bcrypt = require('bcryptjs');
 const User = require('../models/user.model.js');
+const generateToken = require('../utils/generateToken.js');
 
+//login user
 const loginUser = handleAsync(async (req, res) => {
-  res.send('user logged in');
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  //check if user and passsword match
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(StatusCodes.OK).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(StatusCodes.UNAUTHORIZED);
+    throw new Error('Invalid credentials');
+  }
 });
 
 const registerUser = handleAsync(async (req, res) => {
@@ -25,17 +41,18 @@ const registerUser = handleAsync(async (req, res) => {
   const hashedPassword = await bcrypt.hash(password, salt);
 
   //create user
-
   const user = await User.create({ name, email, password: hashedPassword });
   if (user) {
     res.status(StatusCodes.CREATED).json({
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(StatusCodes.BAD_REQUEST);
     throw new error('Invalid user data');
   }
 });
+
 module.exports = { loginUser, registerUser };
